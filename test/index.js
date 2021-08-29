f6d6390c93278a773d28a90b7a51b8fb90f8f443
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 <chosen_one07@outlook.com>
2021-08-29 21:50:47 +0300

    initial commit

---
+++ test/index.js
@@ -0,0 +1,11 @@
+import * as assert from 'assert';
+import Expander from '../src/index';
+
+
+describe('Demo', () => {
+    describe('Describe test', () => {
+        it('it passes', () => {
+            assert.equal(1, 1);
+        });
+    });
+});
